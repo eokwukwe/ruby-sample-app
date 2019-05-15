0944dcffbac7273247502859362de82479ef30f9
Okwukwe Ewurum <okwukwe.ewurum@gmail.com>
2019-05-15 04:36:07 +0100

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
-
-  root 'static_pages#home'
+  root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
@@ -12,4 +10,7 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
+  resources :account_activations, only: [:edit]
 end
